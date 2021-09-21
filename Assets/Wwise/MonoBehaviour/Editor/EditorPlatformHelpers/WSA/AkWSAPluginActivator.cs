feeb053cf78d3396a36eb97f7f6ca89519ae6116
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-09-21 19:10:09 -0400

    Wwise Implemented

---
+++ Assets/Wwise/MonoBehaviour/Editor/EditorPlatformHelpers/WSA/AkWSAPluginActivator.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+[UnityEditor.InitializeOnLoad]
+public class AkWSAPluginActivator
+{
+	static AkWSAPluginActivator()
+	{
+		AkPluginActivator.BuildTargetToPlatformName.Add(UnityEditor.BuildTarget.WSAPlayer, "WSA");
+		AkBuildPreprocessor.BuildTargetToPlatformName.Add(UnityEditor.BuildTarget.WSAPlayer, "Windows");
+	}
+}
+#endif
