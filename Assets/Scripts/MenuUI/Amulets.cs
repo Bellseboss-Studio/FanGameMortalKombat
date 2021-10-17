8c307392d997fa7c868085b5143d36657ff8489f
diosito Gonzalez Marquez <bolo402402@gmail.comname>
2021-10-17 18:26:18 -0500

    Icon from main menu got movement and testing_Angel folder added

--- Assets/Scripts/MenuUI/Amulets.cs
+++ Assets/Scripts/MenuUI/Amulets.cs
@@ -1,36 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using MenuUI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using View.UI;
 
 public class Amulets : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool isOver = false;
-    public Image amuleto;
+    public Image amulet;
+    private float velocity = 10;
+    private IAmuletPositioner _amuletPositioner;
+    public bool amuletIsMoving;
+    private AmuletsMediator _amuletsMediator;
 
-    void Awake() 
+    private void Awake()
     {
-        amuleto.enabled = false;
+        amuletIsMoving = false;
+        amulet.enabled = false;
+        _amuletPositioner = new MoveTowardsAdapter();
     }
 
+    public void Configure(AmuletsMediator amuletsMediator)
+    {
+        _amuletsMediator = amuletsMediator;
+    }
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("Mouse enter");
         isOver = true;
-        if (isOver == true){
-            amuleto.enabled = true;
-        }     
+        if (isOver != true) return;
+        amulet.enabled = true;
+        _amuletsMediator.AnyScriptCanMove();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("Mouse exit");
         isOver = false;
-        if (isOver == false)
-        {
-            amuleto.enabled = false;
-        }
     }
-
+    
+    private void Update()
+    {
+        amuletIsMoving = _amuletPositioner.MoveAmulet(amulet, velocity, gameObject, isOver, amuletIsMoving);
+    }
 }
---
+++ Assets/Scripts/View/UI/AmuletsMediator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace View.UI
+{
+    public class AmuletsMediator : MonoBehaviour
+    {
+        [SerializeField] private Amulets playButton;
+        [SerializeField] private Amulets optionsButton;
+        [SerializeField] private Amulets extrasButton;
+        [SerializeField] private Amulets creditsButton;
+        [SerializeField] private Amulets quitButton;
+
+        private void Awake()
+        {
+            playButton.Configure(this);
+            optionsButton.Configure(this);
+            extrasButton.Configure(this);
+            creditsButton.Configure(this);
+            quitButton.Configure(this);
+        }
+
+        public void AnyScriptCanMove()
+        {
+            playButton.amuletIsMoving = false;
+            optionsButton.amuletIsMoving = false;
+            extrasButton.amuletIsMoving = false;
+            creditsButton.amuletIsMoving = false;
+            quitButton.amuletIsMoving = false;
+        }
+    }
+}
