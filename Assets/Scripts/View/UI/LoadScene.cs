80965a066243fba870087fa60620816fadaef6b4
diosito Gonzalez Marquez <bolo402402@gmail.comname>
2022-02-04 13:23:54 -0600

    boton play funciona

--- Assets/Scripts/View/UI/LoadScene.cs
+++ Assets/Scripts/View/UI/LoadScene.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+namespace View.UI
 {
-    public void loadScene()
+    public class LoadScene : MonoBehaviour
     {
-        SceneManager.LoadScene(1);
-    }
+        public void LoadSceneMethod()
+        {
+            SceneManager.LoadScene(1);
+        }
 
-    public void QuitGame()
-    {
-        Application.Quit();
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
 }
