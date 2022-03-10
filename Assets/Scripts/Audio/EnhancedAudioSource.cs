db6b1b94d1aa77cc0ce5348959e496ecc5a52042
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2022-03-09 22:40:30 -0500

    Implemented Sounds For UI Buttons

--- Assets/Scripts/Audio/EnhancedAudioSource.cs
+++ Assets/Scripts/Audio/EnhancedAudioSource.cs
@@ -10,20 +10,22 @@ public class EnhancedAudioSource : MonoBehaviour, ICheckDependencies
     [SerializeField] [Range(0f, 0.5f)] float pitchVariation;
     [SerializeField] [Range(0f, 0.5f)] float volumeVariation;
     [SerializeField] float maxVolume = 0.7f;
+    [SerializeField] float pitch;
 
     private void Awake()
     {
         CheckDependencies();
         As.playOnAwake = false;
+        pitch = As.pitch;
     }
 
     private void OnEnable()
     {
         int AudioClipIndx = Random.Range(0, Sfx.Count);
         As.clip = Sfx[AudioClipIndx];
-        As.pitch = Random.Range((1f - pitchVariation), (1f + pitchVariation));
+        As.pitch = Random.Range((pitch - pitchVariation), (pitch + pitchVariation));
         As.volume = Random.Range((maxVolume - volumeVariation), maxVolume);
-        As.Play();
+        As.PlayOneShot(As.clip);
     }
 
     public void CheckDependencies()
