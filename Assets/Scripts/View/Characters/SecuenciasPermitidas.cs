670f063e6f0b1271e86b52d02c0f7fc2f7647410
Angel402 <bolo402402@gmail.com>
2022-08-22 19:10:51 -0500

    animaciones aniadidas

---
+++ Assets/Scripts/View/Characters/SecuenciasPermitidas.cs
@@ -0,0 +1,23 @@
+﻿namespace View.Characters
+{
+    public static class SecuenciasPermitidas
+    {
+        public const string ATRAS = "atras";
+        public const string DIAGONALATRAS = "atrasabajo";
+        public const string DELANTE = "delante";
+        public const string DIAGONALDELANTE = "delanteabajo";
+        public const string ABAJO = "abajo";
+        public const string ARRIBA = "arriba";
+        public const string PUNIOFUERTE = "punioFuerte";
+        public const string PUNIODEBIL = "punioDebil";
+        public const string PATADAFUERTE = "patadaFuerte";
+        public const string PATADADEBIL = "patadaDebil";
+        public const string VACIO = "";
+        //poderes!!
+        public const string FIREBALL = "fireball!!!!!!";
+        public const string CORRER = "Correr";
+        public const string SALTARHACIAATRAS = "Saltar hacia atras";
+        public const string DRAGONPUNCH = "dragonPunch!!!";
+
+    }
+}
