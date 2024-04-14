7bd7027fc01f8b88ddb8692a74aaa7e2fa095d34
Angel402 <bolo402402@gmail.com>
2024-04-13 21:47:10 -0600

    first version

---
+++ Assets/Bellseboss/Angel/KeyRebinding/RebindingKeyManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Bellseboss.Angel.KeyRebinding
+{
+    public class RebindingKeyManager : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference moveRef, jumpRef, fireRef;
+
+        private void OnEnable()
+        {
+            moveRef.action.Disable();
+            jumpRef.action.Disable();
+            fireRef.action.Disable();
+        }
+
+        private void OnDisable()
+        {
+            moveRef.action.Enable();
+            jumpRef.action.Enable();
+            fireRef.action.Enable();
+        }
+    }
+}
