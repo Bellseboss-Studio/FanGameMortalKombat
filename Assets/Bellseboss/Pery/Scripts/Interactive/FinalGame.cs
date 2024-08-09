8126e2d50352113919d41fc3fa717f88875c916a
Luis Plata <www.luisplata@gmail.com>
2024-08-09 13:42:32 -0500

    trigger to exit of dungeon is finished; Play a cinematic timeline and after that go to the next scene (cinematic to edenia)

--- Assets/Bellseboss/Pery/Scripts/Interactive/FinalGame.cs
+++ Assets/Bellseboss/Pery/Scripts/Interactive/FinalGame.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinalGame : Activable
 {    
     public override void Activate()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(3);
     }
 }
---
+++ Assets/Scripts/View/UI/PhaseUIGameOver.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PhaseUIGameOver : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI dialogText;
+    [SerializeField] private string[] dialogTexts;
+
+    public void SetDialogToRandom()
+    {
+        dialogText.text = dialogTexts[Random.Range(0, dialogTexts.Length)];
+    }
+}
