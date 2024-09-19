c648be59f7ad6c3727a365f87bc352368a4c619b
Luis Plata <www.luisplata@gmail.com>
2024-09-19 10:59:24 -0500

    Adding shadow to player

--- Assets/Bellseboss/Pery/Scripts/Input/FloorController.cs
+++ Assets/Bellseboss/Pery/Scripts/Input/FloorController.cs
@@ -12,6 +12,9 @@ namespace Bellseboss.Pery.Scripts.Input
         [SerializeField] private bool _isTouchingFloor;
         [SerializeField] private LayerMask layerMask;
         [SerializeField] private List<Transform> raycastsOrigins;
+        [SerializeField] private Transform raycastCenter;
+        [SerializeField] private SpriteRenderer shadow;
+        [SerializeField] private float maxDistance;
         private GameObject _player;
         private bool _isConfigured;
         
@@ -41,6 +44,30 @@ namespace Bellseboss.Pery.Scripts.Input
             _isTouchingFloor = false;
         }
 
+        private void Update()
+        {
+            
+        }
+
+        private void LateUpdate()
+        {
+            UpdateShadow();
+        }
+
+        private void UpdateShadow()
+        {
+            if (Physics.Raycast(raycastCenter.position + Vector3.up * 2, Vector3.down, out var hitToShadow, 100, layerMask))
+            {
+                shadow.gameObject.SetActive(true);
+                float distance = Vector3.Distance(transform.position, hitToShadow.point);
+                float scale = Mathf.Lerp(1.0f, 0.5f, distance / maxDistance);  // Escala de la sombra
+                shadow.transform.localScale = new Vector3(scale, scale, 1f); // Z en 1 ya que es un sprite 2D
+                // Ajusta la transparencia
+                shadow.color = new Color(0f, 0f, 0f, Mathf.Lerp(.6f, 0.2f, distance / maxDistance));
+                shadow.transform.position = new Vector3(shadow.transform.position.x, hitToShadow.point.y + 0.05f, shadow.transform.position.z);
+            }
+        }
+
         public bool IsTouchingFloor()
         {
             return _isTouchingFloor;
@@ -55,8 +82,8 @@ namespace Bellseboss.Pery.Scripts.Input
         private void OnDrawGizmos()
         {
             if(!_isConfigured) return;
-            Gizmos.color = Color.red;
-            Gizmos.DrawRay(_player.transform.position + Vector3.up, Vector3.down * 10);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(raycastCenter.position + Vector3.up * 2, Vector3.down * 10);
             
             Gizmos.color = _isTouchingFloor ? Color.green : Color.red;
             Gizmos.DrawSphere(_player.transform.position + Vector3.up, 0.5f);
